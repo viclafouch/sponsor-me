06e73f1d6dfbb7ec4d608521a7577833e1cb6ebe
viclafouch <victor.dlf@outlook.fr>
2024-12-13 11:36:27 +0100

    fix: bg

--- apps/classpass/src/app/page.tsx
+++ apps/classpass/src/app/page.tsx
@@ -10,7 +10,7 @@ const Home = () => {
       {METAS.GA_ID ? <GoogleAnalytics gaId={METAS.GA_ID} /> : null}
       <div
         style={{ backgroundImage: METAS.MAIN_BACKGROUND_URL }}
-        className="min-h-dvh flex flex-col justify-center items-center bg-main flex-1 bg-cover bg-no-repeat bg-center"
+        className="min-h-dvh flex flex-col justify-center items-center bg-white flex-1 bg-cover bg-no-repeat bg-center"
       >
         <div className="border-black/10 shadow-md border-solid border bg-white py-6 rounded-[20px] flex flex-col gap-6 w-full max-w-2xl">
           <div className="px-10 flex flex-col items-center gap-y-2">
